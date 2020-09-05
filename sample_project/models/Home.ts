2e1339f2911960bacd5f23b8d194793bc8b4d35e
BarthesSimpson <d.s.frenk@gmail.com>
2020-09-04 21:25:52 -0700

    Start building home and mortgage model

--- sample_project/models/Home.ts
+++ sample_project/models/Home.ts
@@ -2,14 +2,14 @@ import Mortgage from "./Mortgage";
 export default class Home {
   name: string;
   #value: number;
-  #initial_value: number;
+  #initialValue: number;
   #equity: number;
   #year: number = 0;
   #mortgage: Mortgage;
 
   constructor(name: string, price: number, mortgage: Mortgage) {
     this.name = name;
-    this.#initial_value = price;
+    this.#initialValue = price;
     this.#value = price;
     this.#mortgage = mortgage;
     this.#equity = mortgage.getDownPayment();
@@ -18,13 +18,49 @@ export default class Home {
   getValue(): number {
     return this.#value;
   }
-}
 
-// double getPercentPaid() const;
-// double getRemainingDebt() const;
-// double getEquity() const;
-// double getMonthlyPayment() const;
-// double computeMonthlyPropertyTax() const;
-// int yearsTillPaid() const;
-// void incrementYear(double appreciationPercent);
-// void makePayment(const double amount);
+  getPercentPaid(): number {
+    return (
+      (this.#initialValue - this.#mortgage.getBalance()) / this.#initialValue
+    );
+  }
+
+  getRemainingDebt(): number {
+    return this.#mortgage.getBalance();
+  }
+
+  getEquity(): number {
+    return (this.#equity / this.#initialValue) * this.#value;
+  }
+
+  yearsTillPaid(): number {
+    if (!this.#mortgage) {
+      return 0;
+    }
+    return this.#mortgage.termInYears - this.#year;
+  }
+
+  getMonthlyPayment(): number {
+    return (
+      this.computeMonthlyPropertyTax() + this.#mortgage.getMonthlyPayment()
+    );
+  }
+
+  computeMonthlyPropertyTax(): number {
+    // TODO: get this from a config file
+    const TAX_RATE = 0.0095;
+    return (TAX_RATE / 12.0) * this.#value;
+  }
+
+  makePayment(amount: number) {
+    // Keep it simple for now by just assuming we always pay an exact multiple of the total monthly payment
+    const numMonths = amount / this.getMonthlyPayment();
+    const tax = numMonths * this.computeMonthlyPropertyTax();
+    this.#mortgage.makePayment(amount - tax);
+    this.#equity = Math.min(
+      this.#equity +
+        numMonths * this.#mortgage.computeMonthlyPrincipalPayment(),
+      this.#initialValue
+    );
+  }
+}
--- sample_project/models/Mortgage.ts
+++ sample_project/models/Mortgage.ts
@@ -1,5 +1,19 @@
 export default class Mortgage {
+  termInYears: number;
+
   getDownPayment(): number {
+    return 100;
+  }
+  getBalance(): number {
+    return 100;
+  }
+  getMonthlyPayment(): number {
+    return 100;
+  }
+  makePayment(amount: number) {
+    return;
+  }
+  computeMonthlyPrincipalPayment(): number {
     return 100;
   }
 }
